60c04e8ce768dfa36143a58578a9438df2790100
Ferney <ferney.echeverri@est.iudigital.edu.co>
2025-05-09 18:48:45 -0500

    linea 19 y linea 23,27,31,35,48,54,62  Pruebas linea 10 y linea 17

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,14 @@ public class Main {
         concesionario.agregarVehiculo(new Vehiculo("001", "Toyota", "Auto", 2020, 0));
         concesionario.agregarVehiculo(new Vehiculo("002", "Chevrolet", "Camioneta", 2018, 50000));
         concesionario.agregarVehiculo(new Vehiculo("003", "Suzuki", "Motocicleta", 2019, 10000));
+        concesionario.agregarVehiculo(new Vehiculo("004", "Renault", "Auto", 2025, 0));
 
         // Mostrar el inventario
         concesionario.mostrarInventario();
 
         // Realizar una venta
         concesionario.realizarVenta("002", 25000, "Pérez", "Juan", "12345678");
+        concesionario.realizarVenta("002", 200000, "Gonzalez", "David", "999999");
 
         // Mostrar el inventario y las ventas
         concesionario.mostrarInventario();
